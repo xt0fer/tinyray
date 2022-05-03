135c5193f3ba82a479e4524b5eeaab7994a75135
Kristofer <kris@youngers.org>
2022-05-03 12:20:23 -0400

    modified to get aspect ratio Correct

--- render.go
+++ render.go
@@ -11,21 +11,21 @@ import (
 )
 
 const (
-	width  = 768
+	width  = 1024
 	height = 768
 )
 
 func render(sphere vector.Sphere) {
 	log.Printf("render size(%d)\n", width*height)
-	framebuffer := [height * width]vector.Vector{}
+	framebuffer := [width * height]vector.Vector{}
 	fov := math.Pi / 2.0
 
 	for j := 0; j < height; j++ {
 		for i := 0; i < width; i++ {
-			// framebuffer[i*height+j] = vector.Vector{
-			// 	X: float64(j) / float64(width),
-			// 	Y: float64(i) / float64(height),
-			// 	Z: 0,
+			// framebuffer[i+j*width] = vector.Vector{
+			// 	X: float64(j) / float64(height),
+			// 	Y: float64(i) / float64(width),
+			// 	Z: 0.0,
 			// }
 			x := float64((2*(float64(i)+0.5)/width - 1) * math.Tan(fov/2.) * width / height)
 			y := -(2*(float64(j)+0.5)/height - 1) * math.Tan(fov/2.)
@@ -37,7 +37,7 @@ func render(sphere vector.Sphere) {
 	scene := engine.NewScene(width, height)
 
 	scene.EachPixel(func(x, y int) color.RGBA {
-		vec := framebuffer[x*height+y]
+		vec := framebuffer[x+y*width]
 		return color.RGBA{
 			uint8(math.Round(vec.X * 255.0)),
 			uint8(math.Round(vec.Y * 255.0)),
@@ -45,7 +45,6 @@ func render(sphere vector.Sphere) {
 			255,
 		}
 	})
-	log.Println("saving")
 	//scene.Save(fmt.Sprintf("./renders/%d.png", time.Now().Unix()))
 	scene.Save(fmt.Sprintf("./renders/foo.png"))
 
